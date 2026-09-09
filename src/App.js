641908048e1d5ae53ba400947a1868b67bc5cafe
Ishita Sahai <ishitasahai0999@gmail.com>
2026-09-09 18:51:37 +0530

    Updated project files, removed unused test and CSS files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Tea from './Tea'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+  <h1>tea and react | </h1>
+  <Tea/>
+  </>
   );
 }
 
 export default App;
+
+//the thing that's to b learnt is that:- 
+//1) when we make any components ie. Tea function that we are exporting here or any other component then we should always name it by capitalizing it and the names of the files should also be capitalized file name capitalizing is just a good practice it won't give error anyway but still it's good to use capitalized names for the file too
+//2) some libraries force to have the file name having components with .jsx  especially if any html is getting returned in it 
+//3) in createreact apps:- if any html is getting returned then we keep the file name as .jsx compulsorily otherwise we can use .js as well 
+//in vite:- mostly by default the file name is .jsx 
+//in both we should name the component names by capitalizing and also it's a good practice to keep the file name that contains the component also by capitalizing 
+//
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import {StrictMode} from 'react';//core foundational library that takes all the references 
+import {createRoot} from 'react-dom/client';//implementation of react on web
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+//reacts creates its own DOM (tree stucture) which is k/a a virtual DOM then its compared with main DOM then only some of the elements are changed in the main DOM that are needed and not all of the elements 
+const root = createRoot(document.getElementById('root'));
+root.render(//reactDOM has a method createroot to create root at the specified place ie. taken by the parameter mentioned in the method 
+  //the reference of the element is then taken into the variable root then root is asked to render the App from strict mode of react just as the main browser's DOM renders the html similarly it renders the html here 
+  //now as there's no such tag like App but here we're able to create our own custom tags 
+  // <StrictMode>{/*creates a dom and renders*/}
+    <App />/*we are creating our custom tags, js k via html el ko render kr skte h by using the poewer of jsx , here app is a function that returns html and then it's exported in the App.js file so the thing here is that react gives a syntax in which we can make a function and return html and then it is rendered via react so we can write html via js*/
+  // </StrictMode>/*this is the safe mode of react that helps to do optimizations in case if we want during development specific tasks //even if we remove it the code will work as it's without any problem*/
 );
+// we've got programming capabilities in html which weren't there previously 
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//in case of complex UIs this concept becomes imp
